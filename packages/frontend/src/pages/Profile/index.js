f8c55bdb6e0ee31a29a5d73c31ecbf0bc32a89ae
Alley M. Carvalho <alleycarvalho@gmail.com>
2020-03-30 22:13:56 -0300

    feat(frontend): logout functionality

--- packages/frontend/src/pages/Profile/index.js
+++ packages/frontend/src/pages/Profile/index.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import { FiPower, FiTrash2 } from 'react-icons/fi';
 
 import api from '../../services/api';
@@ -12,6 +12,8 @@ export default function Profile() {
   const [incidents, setIncidents] = useState([]);
   const ongId = localStorage.getItem('ongId');
   const ongName = localStorage.getItem('ongName');
+
+  const history = useHistory();
 
   useEffect(() => {
     api.get('profile', {
@@ -37,6 +39,12 @@ export default function Profile() {
     }
   }
 
+  function handleLogout() {
+    localStorage.clear();
+
+    history.push('/');
+  }
+
   return (
     <div className="profile-container">
       <header>
@@ -47,7 +55,10 @@ export default function Profile() {
           Cadastrar novo caso
         </Link>
 
-        <button type="button">
+        <button
+          type="button"
+          onClick={handleLogout}
+        >
           <FiPower size={18} color="#E02041" />
         </button>
       </header>
