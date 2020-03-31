1b2c6b5223d06a5dbe80b0bc7d7baf649d122fb2
Alley M. Carvalho <alleycarvalho@gmail.com>
2020-03-31 13:54:55 -0300

    feat(mobile): features to open whatsapp and email

--- packages/mobile/src/pages/Details/index.js
+++ packages/mobile/src/pages/Details/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { Feather } from '@expo/vector-icons';
 import { useNavigation } from '@react-navigation/native';
-import { View, Image, Text, TouchableOpacity } from 'react-native';
+import { View, Image, Text, TouchableOpacity, Linking } from 'react-native';
+import * as MailComposer from 'expo-mail-composer';
 
 import logoImg from '../../assets/logo.png';
 
@@ -9,9 +10,22 @@ import styles from './styles';
 
 export default function Details() {
   const navigation = useNavigation();
+  const message = 'Olá ONG, estou entrando em contato, pois gostaria de ajudar no caso "Título do caso" com o valor de R$ 120,00';
 
   function navigateBack() {
     navigation.goBack();
+  }
+
+  function sendWhatsapp() {
+    Linking.openURL(`whatsapp://send?phone=34999999999&text=${message}`);
+  }
+
+  function sendMail() {
+    MailComposer.composeAsync({
+      subject: 'Herói do caso: Título do caso',
+      recipients: ['contato@gmail.com'],
+      body: message,
+    });
   }
 
   return (
@@ -69,7 +83,7 @@ export default function Details() {
         <View style={styles.actions}>
           <TouchableOpacity
             style={styles.action}
-            onPress={() => {}}
+            onPress={sendWhatsapp}
           >
             <Text style={styles.actionText}>
               WhatsApp
@@ -78,7 +92,7 @@ export default function Details() {
 
           <TouchableOpacity
             style={styles.action}
-            onPress={() => {}}
+            onPress={sendMail}
           >
             <Text style={styles.actionText}>
               E-mail
